dc06c5f030c52b5792596b74c67de9e79ab42bed
Eliminioa <eliminioa@gmail.com>
2015-08-02 18:57:00 -0400

    Finalize listen process and rename sensors.py
    Rename sensors.py to botIO.py to better state its purpose, since
    it now does replies to sign ups as well as listening for them. Also,
    I finished the listen method in the Mind class, so in theory it should
    now be able to recruit players from the sign up threads.
    
    Signed-off-by: Eliminioa <eliminioa@gmail.com>

---
+++ Body/botIO.py
@@ -0,0 +1,120 @@
+import re
+
+import praw.objects
+
+from Mind import memory
+from Utilities import CASexcepts as excs
+from Utilities.loggingSetup import create_logger
+"""
+Module for letting the Chroma Automation Suite sense users both signing up
+to receive alerts from their generals and to sense which side players are on.
+"""
+LOG = create_logger(__name__)
+
+
+def recruit_getter(cfg, db, antenna, side):
+    """
+    Get all of the new recruits from the proper side's recruitment thread
+    and return them in a list. Note that this will only return new
+    recruits.
+
+    :param cfg: Global configuration information to be used
+    :param db: Database to be used
+    :param antenna: Antenna connection to Reddit
+    :param side: Side to gather recruits from
+    :return: A list of new recruits.
+    """
+    # retrieve majors from DB
+    majors = memory.get_players_with(db, side=side, recruited=True)
+
+    # retrieve sign up thread from reddit
+    if side == 0:
+        signup_thread = antenna.get_submission(
+            submission_id=cfg['OR_RECRUIT_THREAD'],
+            comment_limit=None,
+            comment_sort='new')
+    elif side == 1:
+        signup_thread = antenna.get_submission(
+            submission_id=cfg['PW_RECRUIT_THREAD'],
+            comment_limit=None,
+            comment_sort='new')
+    else:
+        raise excs.InvalidSideError(__name__, side)
+    LOG.debug('Got signup thread for side {} from {}'.format(
+        side,
+        signup_thread.id))
+    # and replace more comments
+    signup_thread.replace_more_comments()
+    LOG.debug('Replaced more comments')
+
+    # list comment authors from comment generator
+    all_recruits = [cmnt for cmnt in signup_thread.comments]
+
+    # filter out already registered recruits
+    new_recruits = [recr for recr in all_recruits if
+                    str(recr.author) not in majors]
+
+    return new_recruits
+
+
+def retrieve_combatants(antenna):
+    """
+    Look through old battles and skirmishes to retrieve users that have
+    battled and their respective sides. Maybe later also keep track of
+    troop counts and attendance?
+
+    :param antenna: Antenna connection to Reddit
+    :return: A dictionary of usernames and their side
+    """
+    # retrieve bot comments
+    bot = antenna.get_redditor('chromabot')
+    bot_content = antenna.get_content(url=bot._url, limit=None)
+    comments = [item for item in bot_content if isinstance(item, praw.objects.Comment)]
+
+    # find skirmish comments
+    skirms = [thread for thread in comments if "Confirmed actions for this skirmish:" in thread.body]
+
+    # regex pattern to extract name and side
+    idPattern = re.compile(r"\w+\s+\(+\w+\)")
+
+    combatant_dict = {}
+
+    # look through each skirm post and extract players
+    for skirm in skirms:
+        players = idPattern.findall(skirm.body)
+
+        #go through each player and add or update dict entry
+        for player in players:
+            # username is the first part of player
+            username = player.split()[0]
+            # side is the second part
+            side = player.split()[1]
+
+            if side == 'Orangered':
+                combatant_dict[username] = 0
+            elif side == 'Perwinkle':
+                combatant_dict[username] = 1
+            else:
+                raise excs.InvalidSideError(__name__, side)
+    return combatant_dict
+
+
+def reply_to_signup(sign_up, side, cfg):
+    """
+    As labeled, replies to a sign up with the appropriate message
+
+    :param sign_up: Top-level commment to reply to
+    :param side: Side the bot represents
+    :param cfg: Config to be used
+
+    :return: True if successful
+    """
+    if side == 0:
+        message = cfg['OR_RECRUIT_RESPONSE']
+    elif side == 1:
+        message = cfg['PW_RECRUIT_RESPONSE']
+    else:
+        raise excs.InvalidSideError(__name__, side)
+
+    # reply with a customized message
+    sign_up.reply(message.format(str(sign_up.author)))
--- Mind/mind.py
+++ Mind/mind.py
@@ -3,15 +3,17 @@ import time
 import sqlite3
 from multiprocessing import Process
 
+import praw
+
 import memory
-from Body import sensors
+from Body import botIO
 from Utilities.loggingSetup import create_logger
 
 class Mind(Process):
     """
-    A class for the mind of Prime or ORHQ. Lets them do their "thinking" i.e.
-    get new users from the sign-ups, figure out which side a user is on, and
-    in the future maybe do some battle-related things.
+    A class for the mind of Prime or ORHQ. Lets them do their "thinking"
+    i.e. get new users from the sign-ups, figure out which side a user is
+    on, and in the future maybe do some battle-related things.
     """
 
     def __init__(self, name, config, antenna, nerves):
@@ -27,32 +29,64 @@ class Mind(Process):
         while True:
             try:
                 self.log.debug("Began MIND process")
-                self.think()
+                self.listen()
             except:
                 exctype, value = sys.exc_info()[:2]
-                self.log.error("Error in thinking: {}: {}".format(exctype, value))
+                self.log.error("Error in thinking: {}: {}".format(
+                    exctype,
+                    value))
 
 
-    def think(self):
+    def listen(self):
+        """
+        The actual workflow of monitoring sign ups and user involvement.
+        Right now that means getting recruits from the sign up thread
+        and figuring out which side people are on.
+
+        :return: Nothing as of now
+        """
         user = None
         side = None
         self.log.info("Began thinking")
         signal_received = self.nerves.poll(None)
         while signal_received:
-            # choose which side to work with
-            # use sensors.get_recruit_commenters to get new top-level comment authors
-            # pass list of new top-level commenters to self.register_recruits
-            # scan Chromabot's battle history with sensors.retrieve_combatants and receive a list of users and their side
-            # pass this list through memory.handle_player_memory to store user sides
-            # TODO See if I can keep track of user troop counts and battle counts too
-            # TODO Aggregate lore from Chromalore and battles and store it in the DB
+            # TODO Keep track of user troop counts and battle counts too
+            # TODO Aggregate lore from Chromalore and battles and store it
 
             #set antenna to correct side
-            user = ('Orangered_HQ' if user == 'Periwinkle_Prime_3' else 'Periwinkle_Prime_3')
+            user = ('Orangered_HQ' if user == 'Periwinkle_Prime_3'
+                    else 'Periwinkle_Prime_3')
             side = (0 if side == 1 else 1)
             self.antenna.set_user(user)
             self.log.info("Set antenna to {}".format(user))
-            sensors.recruit_getter(self.cfg, self.antenna, self.db, side)
-            time.sleep(2)
 
-            # sensors.get_recruit_commenters(side)
+            # use sensors.get_recruit_commenters to get new top-level comments
+            new_recruits = botIO.recruit_getter(self.cfg,
+                                                  self.antenna,
+                                                  self.db,
+                                                  side)
+
+            # handle the recruits
+            for recruit in new_recruits:
+                memory.handle_player_memory(self.db,
+                                            str(recruit.author),
+                                            side=side,
+                                            recruited=True)
+
+                # if the bot hasn't already replied, then do so
+                if user not in [str(rep.author) for rep in recruit.replies
+                                if not isinstance(rep,
+                                        praw.objects.MoreComments)]:
+                    botIO.reply_to_signup(recruit, side, self.cfg)
+
+            # scan battle history with sensors.retrieve_combatants and
+            # receive a dict of users and their side
+            combatant_dict = botIO.retrieve_combatants(self.antenna)
+
+            # handle combatants
+            for combatant in combatant_dict:
+                memory.handle_player_memory(self.db,
+                                            combatant,
+                                            side=combatant_dict[combatant])
+
+            time.sleep(2)
