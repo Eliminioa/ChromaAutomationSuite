61dbbd6705a9a8e5c1e8afaf21655cff842d63a9
Eliminioa <eliminioa@gmail.com>
2015-08-06 15:44:07 -0400

    Clean up mind.py code
    Signed-off-by: Eliminioa <eliminioa@gmail.com>

--- Mind/mind.py
+++ Mind/mind.py
@@ -10,6 +10,7 @@ import memory
 from Body import botIO
 from Utilities.loggingSetup import create_logger
 
+
 class Mind(Process):
     """
     A class for the mind of Prime or ORHQ. Lets them do their "thinking"
@@ -41,6 +42,69 @@ class Mind(Process):
                     value))
                 raise
 
+    def get_recruits(self, user, side):
+        """
+        Get and handle new recruits from sign up theads.
+
+        :param user: username of active bot
+        :param side: side of active bot
+        :return: list of new recruits
+        """
+        # use sensors.get_recruit_commenters to get new top-level comments
+        # lock stuff down while handling the DB
+        self.lock.acquire()
+        new_recruits = botIO.recruit_getter(self.cfg,
+                                            self.db,
+                                            self.antenna,
+                                            side)
+        self.lock.release()
+        self.log.info("Retrieved {} new recruits".format(
+            len(new_recruits)))
+
+        # handle the recruits
+        for recruit in new_recruits:
+            self.lock.acquire()
+            memory.handle_player_memory(self.db,
+                                        str(recruit.author),
+                                        side=side,
+                                        recruited=True)
+            self.lock.release()
+            self.log.info("Handled player {} of side {}".format(
+                str(recruit.author), side))
+            print("Handled player {} of side {}".format(
+                str(recruit.author), side))
+
+            # if the bot hasn't already replied, then do so
+            if user not in [str(rep.author) for rep in recruit.replies
+                            if not isinstance(rep,
+                                              praw.objects.MoreComments)]:
+                botIO.reply_to_signup(recruit, side, self.cfg)
+                self.log.debug("Replied to player {}".format(
+                    str(recruit.author), side))
+
+    def get_combatants(self, user, side):
+        # scan battle history with sensors.retrieve_combatants and
+        # receive a dict of users and their side
+        self.lock.acquire()
+        combatant_dict = botIO.retrieve_combatants(self.antenna)
+        self.lock.release()
+
+        # handle combatants
+        for combatant in combatant_dict:
+            self.lock.acquire()
+            memory.handle_player_memory(self.db,
+                                        combatant,
+                                        side=combatant_dict[combatant])
+            self.lock.release()
+
+            # update the
+
+        # refresh bot's token
+        new_access_info = self.antenna.refresh_token_user()
+        memory.handle_player_memory(self.db,
+                                    user,
+                                    accessInfo=dumps(new_access_info))
+
     def listen(self):
         """
         The actual workflow of monitoring sign ups and user involvement.
@@ -62,64 +126,15 @@ class Mind(Process):
             # TODO Aggregate lore from Chromalore and battles and store it
 
             #set antenna to correct side
-            # DEBUG user = ('Orangered_HQ' if user == 'Periwinkle_Prime_3'
-            # DEBUG       else 'Periwinkle_Prime_3')
-            # DEBUG side = (0 if side == 1 else 1)
+            user = ('Orangered_HQ' if user == 'Periwinkle_Prime_3'
+                  else 'Periwinkle_Prime_3')
+            side = (0 if side == 1 else 1)
             self.antenna.set_user(user)
             self.log.info("Set antenna to {}".format(user))
 
-            # use sensors.get_recruit_commenters to get new top-level comments
-            # lock stuff down while handling the DB
-            self.lock.acquire()
-            new_recruits = botIO.recruit_getter(self.cfg,
-                                                self.db,
-                                                self.antenna,
-                                                side)
-            self.lock.release()
-            self.log.info("Retrieved {} new recruits".format(
-                len(new_recruits)))
+            self.get_recruits(user, side)
 
-            # handle the recruits
-            for recruit in new_recruits:
-                self.lock.acquire()
-                memory.handle_player_memory(self.db,
-                                            str(recruit.author),
-                                            side=side,
-                                            recruited=True)
-                memory.add_player(side, 'all', str(recruit.author))
-                self.lock.release()
-                self.log.info("Handled player {} of side {}".format(
-                    str(recruit.author), side))
-                print("Handled player {} of side {}".format(
-                    str(recruit.author), side))
-
-                # if the bot hasn't already replied, then do so
-                if user not in [str(rep.author) for rep in recruit.replies
-                                if not isinstance(rep,
-                                        praw.objects.MoreComments)]:
-                    botIO.reply_to_signup(recruit, side, self.cfg)
-                    self.log.debug("Replied to player {}".format(
-                        str(recruit.author), side))
-
-            # scan battle history with sensors.retrieve_combatants and
-            # receive a dict of users and their side
-            self.lock.acquire()
-            combatant_dict = botIO.retrieve_combatants(self.antenna)
-            self.lock.release()
-
-            # handle combatants
-            for combatant in combatant_dict:
-                self.lock.acquire()
-                memory.handle_player_memory(self.db,
-                                            combatant,
-                                            side=combatant_dict[combatant])
-                self.lock.release()
-
-            # refresh bot's token
-            new_access_info = self.antenna.refresh_token_user()
-            memory.handle_player_memory(self.db,
-                                        user,
-                                        accessInfo=dumps(new_access_info))
+            self.get_combatants(user, side)
 
             print("Done with {}'s cycle".format(user))
             time.sleep(30)
